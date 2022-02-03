e40b5414f94a63aa26e422fda822b4e5b125f69b
joaoluis12 <joaoluis.lourenco12@gmail.com>
2022-02-03 20:23:22 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const tarefa = document.getElementById('tarefa');
+var formCheck = document.querySelector('.form-check');
+var novaTarefa = '';
+var i=0;
+// var adicionar = document.getElementById('bt-adicionar');
+
+function inserirTarefa(){
+    let tempTarefa = tarefa.value;  
+
+    if(novaTarefa == ''){
+        novaTarefa = '<input type="checkbox" value="" id="ck'+i+'" name="" id=""><label for="ck'+i+'"> ' + tempTarefa + '</label>';
+    } else {
+        novaTarefa = novaTarefa + '<br> <input type="checkbox" value="" id="ck'+i+'" name="" id=""><label for="ck'+i+'"> ' + tempTarefa + '</label>';
+    }
+    formCheck.innerHTML = novaTarefa;
+    tarefa.value = '';
+}
+
+// adicionar.addEventListener('click', inserirTarefa());
